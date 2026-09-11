fb9e38e56f2f9e3ce9045f499709b1c86d7f9e14
konghjin <dusen380@gmail.com>
2026-09-11 18:09:38 +0900

    260911 리다이렉트 재수정

--- src/router/index.js
+++ src/router/index.js
@@ -17,7 +17,7 @@ const router = createRouter({
     // ==========================================
     {
       path: '/:pathMatch(.*)*',
-      redirect: '/birthday',
+      redirect: '/birthday/',
     },
   ],
 })
